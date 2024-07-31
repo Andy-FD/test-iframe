cda383be2cf541f0f9c6c5580796adc1a5869252
Andy Wörner <Andy@frozendonkey.com>
2024-07-31 10:13:48 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex">
+    <div class="flex-grow border-2 border-red-300">
+      <iframe
+        class="h-full w-full"
+        src="https://allthingsdesignsystem.substack.com/embed"
+        frameborder="5"
+        scrolling="no"
+      ></iframe>
+    </div>
+  </div>
+</template>
